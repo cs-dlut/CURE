76726858def82049c00e3c81bb4b33d6206fd972
juan li <52186085+Lijuan-css@users.noreply.github.com>
2022-04-28 13:43:06 +0800

    Add files via upload

---
+++ simulation experiments/codes/stochastic prisoner's dilemma games/DataInter.java
@@ -0,0 +1,9 @@
+package SG.evolution;
+public class DataInter {
+	public static int iii=16;
+	public static double[][] payoff = new double[iii][iii];
+	public static double[][] cooperate = new double[iii][iii];
+	public static double[] ratio = new double[iii];
+	public static double[] fitness = new double[iii];
+	public static double[] CoopeRate = new double[iii];
+}
