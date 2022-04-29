2380bb66df01695e4c3930b1188f082e03d0e3f2
juan li <52186085+Lijuan-css@users.noreply.github.com>
2022-04-29 11:48:33 +0800

    Delete simulation experiments/codes/stochastic prisoner's dilemma games directory

--- simulation experiments/codes/stochastic prisoner's dilemma games/DataInter.java
+++
@@ -1,9 +0,0 @@
-package SG.evolution;
-public class DataInter {
-	public static int iii=16;
-	public static double[][] payoff = new double[iii][iii];
-	public static double[][] cooperate = new double[iii][iii];
-	public static double[] ratio = new double[iii];
-	public static double[] fitness = new double[iii];
-	public static double[] CoopeRate = new double[iii];
-}
