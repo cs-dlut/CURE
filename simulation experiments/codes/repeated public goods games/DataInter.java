66d8721e5d5b48479f17f9753a6b2febfebbbbc1
juan li <52186085+Lijuan-css@users.noreply.github.com>
2022-04-29 11:46:52 +0800

    Delete simulation experiments/codes/repeated public goods games directory

--- simulation experiments/codes/repeated public goods games/DataInter.java
+++
@@ -1,9 +0,0 @@
-package PGG.evolution;
-public class DataInter {
-	public static int iii=257;
-	public static double[][] payoff = new double[iii][iii];
-	public static double[][] cooperate = new double[iii][iii];
-	public static double[] ratio = new double[iii];
-	public static double[] fitness = new double[iii];
-	public static double[] CoopeRate = new double[iii];
-}
